c1e15d6e6c1693edecd6637e1a91b789ee0b7662
Keith Piong <keith.piong@gmail.com>
2016-10-03 12:56:04 +0800

    backupDontMerge

--- Cast/DescriptionViewController.swift
+++ Cast/DescriptionViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class DescriptionViewController: UIViewController {
 
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +23,4 @@ class DescriptionViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
