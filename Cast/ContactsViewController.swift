99ed1751aeadaceda2f0e74081c87d052d832ce3
ggezzz <ggezzz@yahoo.com>
2016-10-10 16:55:52 +0800

    1-1chat in progress

--- Cast/ContactsViewController.swift
+++ Cast/ContactsViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ContactsViewController: UITableViewController {
 
-    var contactList = ["raphael","keith","fz"]
-    
+    var contactList = [User]()
+    var sender : User!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -34,58 +35,20 @@ class ContactsViewController: UITableViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier=="chatSegue" {
         super.prepare(for: segue, sender: sender)
         let chatVc = segue.destination as! ChatViewController // 1
         //_ = navVc.viewControllers.first as! ChatViewController // 2
         if let selectedRow = self.tableView.indexPathForSelectedRow?.row {
-            chatVc.senderId = self.contactList[selectedRow] // 3
-            chatVc.senderDisplayName = self.contactList[selectedRow] // 4
+            chatVc.senderId = sender.userUID // 3
+            chatVc.senderDisplayName = sender.username // 4
+            chatVc.receiver = self.contactList[selectedRow]
+            
+        }
         }
     }
     
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
