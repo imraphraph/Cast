ec2029c5754a5c73e0f006c6249d8a4372d463d8
“Raphael” <raphaellim.creative@gmail.com>
2016-10-11 11:54:43 +0800

    added user profile

--- Cast/ContactsViewController.swift
+++ Cast/ContactsViewController.swift
@@ -11,17 +11,17 @@ import Firebase
 import FirebaseDatabase
 
 class ContactsViewController: UITableViewController {
-
+    
     var contactList = [User]()
     var sender : User!
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loadUser()
         
-
-         }
+        
+    }
     
     func loadUser() {
         
@@ -32,9 +32,9 @@ class ContactsViewController: UITableViewController {
                     self.tableView.reloadData()
                 }
             }
-        }
+            }
         )}
-
+    
     override func viewDidAppear(_ animated: Bool) {
         
         
@@ -43,12 +43,12 @@ class ContactsViewController: UITableViewController {
         
         
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return self.contactList.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "chatCell", for: indexPath) as? ContactsCell
@@ -59,27 +59,41 @@ class ContactsViewController: UITableViewController {
         let imageUrl = NSURL(string:user.profilePhotoURL!)
         cell?.profileImage.sd_setImage(with: imageUrl as URL!)
         
-
+        
         return cell!
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "profileSegue", sender: self)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-        if segue.identifier=="chatSegue" {
-        super.prepare(for: segue, sender: sender)
-        let chatVc = segue.destination as! ChatViewController // 1
+        if segue.identifier == "profileSegue" {
+            super.prepare(for: segue, sender: sender)
+            let destination = segue.destination as! UserProfile
+            if let selectedIndex = self.tableView.indexPathForSelectedRow{
+                let user = contactList[selectedIndex.row]
+                destination.userUID = user.userUID
+            }
+        }
+        
+        
+        //        if segue.identifier=="chatSegue" {
+        //        super.prepare(for: segue, sender: sender)
+        //        let chatVc = segue.destination as! ChatViewController // 1
         //_ = navVc.viewControllers.first as! ChatViewController // 2
-        if let selectedRow = self.tableView.indexPathForSelectedRow?.row {
-//            chatVc.senderId = sender.userUID // 3
-//            chatVc.senderDisplayName = sender.username // 4
-//            chatVc.receiver = self.contactList[selectedRow]
-            
-        }
-//            chatVc.senderId = self.contactList[selectedRow] // 3
-//            chatVc.senderDisplayName = self.contactList[selectedRow] // 4
-        }
+        //        if let selectedRow = self.tableView.indexPathForSelectedRow?.row {
+        //            chatVc.senderId = sender.userUID // 3
+        //            chatVc.senderDisplayName = sender.username // 4
+        //            chatVc.receiver = self.contactList[selectedRow]
+        
+        //        }
+        //            chatVc.senderId = self.contactList[selectedRow] // 3
+        //            chatVc.senderDisplayName = self.contactList[selectedRow] // 4
+        //        }
     }
     
     
-
+    
 }
---
+++ Cast/UserProfile.swift
@@ -0,0 +1,97 @@
+//
+//  UserProfile.swift
+//  Cast
+//
+//  Created by Raphael Lim on 11/10/2016.
+//  Copyright © 2016 com.cast. All rights reserved.
+//
+
+import UIKit
+
+class UserProfile: UIViewController {
+
+    @IBOutlet weak var descriptionTextField: UILabel!
+    @IBOutlet weak var genderTextField: UILabel!
+    @IBOutlet weak var locationTextField: UILabel!
+    @IBOutlet weak var titleTextField: UILabel!
+    @IBOutlet weak var usernameTextField: UILabel!
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    var userUID : String = ""
+    var portfolioLink : String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loadUserInfo()
+        retrieveProfileImage()
+    }
+
+    func loadUserInfo () {
+    
+        DataService.userRef.child(userUID).observe(.value, with: { (snapshot) in
+            let userDictionary: [String: AnyObject]? = snapshot.value as? [String: AnyObject]
+            if let username = userDictionary?["username"] as? String{
+                self.usernameTextField.text = username
+            }
+            if let title = userDictionary?["Role"] as? String{
+                self.titleTextField.text = title
+            }
+            if let gender = userDictionary?["Gender"] as? String{
+                self.genderTextField.text = gender
+            }
+            if let description = userDictionary?["ProfileDescription"] as? String{
+                self.descriptionTextField.text = description
+            }
+            if let portfolio = userDictionary?["PortfolioLink"] as? String{
+                self.portfolioLink = portfolio
+            }
+            if let location = userDictionary?["Location"] as? String {
+                self.locationTextField.text = location
+            }
+        })
+        
+    }
+    
+    func retrieveProfileImage (){
+        
+        DataService.userRef.child(userUID).child("profilePicture").observe(.value, with: {(snapshot) in
+            
+            
+            if let profileImageLink = snapshot.value as? String {
+                let profileImageUrl = NSURL(string: profileImageLink)
+                URLSession.shared.dataTask(with: profileImageUrl as! URL, completionHandler: {
+                    (data, response, error) in
+                    
+                    if error != nil{
+                        print(error)
+                        return
+                    }
+                    
+                    let image = UIImage(data: data!)
+                    DispatchQueue.main.async(execute: {
+                        self.profileImage.image = image
+                    })
+                    
+                }).resume()
+            }
+        })
+    }
+
+    
+    @IBAction func portfolioButton(_ sender: AnyObject) {
+        
+        if self.portfolioLink.lowercased() .hasPrefix("http://"){
+            let myURL = self.portfolioLink
+            UIApplication.shared.openURL(URL(string: myURL)!)
+        } else if self.portfolioLink.lowercased() .hasPrefix("https://"){
+            let myURL = self.portfolioLink
+            UIApplication.shared.openURL(URL(string: myURL)!)
+        } else {
+            let myURL = "http://" + self.portfolioLink
+            UIApplication.shared.openURL(URL(string: myURL)!)
+        }
+        
+    
+    }
+}
